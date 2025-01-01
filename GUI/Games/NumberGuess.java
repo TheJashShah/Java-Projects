436f01e198941d12a951a45ba1290055ad371601
JashShahisOK <jash.shah23@spit.ac.in>
2025-01-01 21:48:27 +0530

    update

--- GUI/Games/NumberGuess.java
+++ GUI/Games/NumberGuess.java
@@ -1,4 +1,4 @@
-package src;
+//package src;
 //-----Imports-----//
 import java.awt.Font;
 import java.awt.GridLayout;
--- GUI/Games/RPS.java
+++ GUI/Games/RPS.java
@@ -1,4 +1,4 @@
-package src;
+//package src;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
